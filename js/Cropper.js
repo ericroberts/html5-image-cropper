382c4b8f0416bcda9544884b5e6415e4273fab68
Eric Roberts <ericroberts@gmail.com>
2013-05-01 10:21:28 -0400

    Allowing dragging and dropping of images

---
+++ js/Cropper.js
@@ -0,0 +1,84 @@
+(function() {
+  function Cropper() {
+    this.init = init;
+
+    var placeholder = document.getElementById('placeholder'),
+        toolbar = document.getElementById('toolbar'),
+        body = document.getElementsByTagName('body')[0],
+        viewfinder = {
+          width: window.innerWidth,
+          height: window.innerHeight
+        }
+        img = new Image(),
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        handle = {
+          size: 60,
+          drag: [false,false,false,false],
+          touch: [0,0,0,0],
+          all: false
+        },
+        selection: null;
+
+    function init() {
+      eventBindings();
+    }
+
+    function eventBindings() {
+      placeholder.addEventListener('drop', drop, false);
+      placeholder.addEventListener('dragover', hover, false);
+      placeholder.addEventListener('dragleave', hover, false);
+
+      window.onresize = function(event) {
+        viewfinder.width = window.innerWidth;
+        viewfinder.height = window.innerHeight;
+
+        console.log(viewfinder);
+      }
+    }
+
+    function hover(event) {
+      event.stopPropagation();
+      event.preventDefault();
+      event.target.className = (event.type == "dragover" ? "hover" : "");
+    }
+
+    function drop(event) {
+      hover(event);
+
+      var files = event.target.files || event.dataTransfer.files,
+          file = files[0],
+          reader = new FileReader();
+
+      reader.onload = createCanvas;
+      reader.readAsDataURL(file);
+    }
+
+    function createCanvas(event) {
+      img.onload = function() {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.width = img.width;
+        ctx.height = img.height;
+
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        canvas.setAttribute('style', 'margin: -'+canvas.height/2+'px 0 0 -'+canvas.width/2+'px;');
+
+        body.appendChild(canvas);
+        canvas.className = 'visible';
+        placeholder.className = 'hidden';
+        toolbar.className = 'visible';
+      }
+      img.src = event.target.result;
+    }
+
+    function drawSelection
+  }
+  
+  var cropper = new Cropper();
+
+  if(window.File && window.FileList && window.FileReader) {
+    cropper.init();
+  }
+})();
